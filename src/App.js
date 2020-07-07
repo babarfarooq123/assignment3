b70d76b19d7d569ba74dcd4148f0481b48376ae6
babarfarooq123 <48159423+babarfarooq123@users.noreply.github.com>
2020-07-07 19:24:13 +0500

    parent to child data passing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Meals from './meals';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Below is our Meals that we serve in our Restaurant</h1>
+      <Meals dish1="Biryani" dish2="Nihari" dish3="Chapli kabab"/>
     </div>
   );
 }
---
+++ src/meals.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Meals(props){
+    return (
+        <div className="Meals">
+            <h1>DISH # 1 = {props.dish1}</h1>
+            <h1>DISH # 2 = {props.dish2}</h1>
+            <h1>DISH # 3 = {props.dish3}</h1>
+        </div>
+    );
+}
+
+export default Meals;
