98b05b1dd7a7c0c13de63453909a7693f2392360
babarfarooq123 <48159423+babarfarooq123@users.noreply.github.com>
2020-07-07 19:40:52 +0500

    again changes meals file

--- src/meals.js
+++ src/meals.js
@@ -5,7 +5,7 @@ function Meals(props){
         <div className="Meals">
             <h1>DISH # 1 = {props.dish1}</h1>
             <h1>DISH # 2 = {props.dish2}</h1>
-            <h1>DISH  = {props.dish3}</h1>
+            <h1>DISH # 3 = {props.dish3}</h1>
         </div>
     );
 }
